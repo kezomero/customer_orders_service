76450ea36c7576ae51bf075111f153e0fa78f7f8
@Derrick <dimatata01@gmail.com>
2025-04-20 10:06:41 +0300

    update

--- api/serializers.py
+++ api/serializers.py
@@ -4,25 +4,24 @@ from .models import Customer, Order
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'name', 'code', 'phone']
+        fields = ['id', 'name', 'code', 'email', 'phone', 'location', 'joined_at']
         extra_kwargs = {
             'code': {'min_length': 3},
             'phone': {'min_length': 10}
         }
 
     def validate_phone(self, value):
-        # Optional: normalize and reformat phone number
         if value.startswith('07'):
-            value = '+254' + value[1:]
+            return '+254' + value[1:]
         elif value.startswith('254'):
-            value = '+' + value
-        elif not value.startswith('+254'):
-            raise serializers.ValidationError("Phone number must be a valid Kenyan number.")
-        return value
+            return '+' + value
+        elif value.startswith('+254'):
+            return value
+        raise serializers.ValidationError("Phone number must be a valid Kenyan number.")
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'item', 'amount', 'created_at']
+        fields = ['id', 'customer', 'item', 'quantity', 'amount', 'payment_method', 'created_at']
         read_only_fields = ['created_at']
