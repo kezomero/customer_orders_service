2f44a1ce1a72e222799e73fc95121c778c095660
@Derrick <dimatata01@gmail.com>
2025-04-21 15:36:21 +0300

    update

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, OrderViewSet
+from .views import (
+    CustomerViewSet,
+    OrderViewSet,
+    CustomLoginView,
+    CustomOIDCAuthenticationCallbackView
+)
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
