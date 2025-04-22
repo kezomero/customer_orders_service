120d13175961ac8808d8209f24e2ac438565d4f9
@Derrick <dimatata01@gmail.com>
2025-04-22 19:55:51 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_NAME'),
+        'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
