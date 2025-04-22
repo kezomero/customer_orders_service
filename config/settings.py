7aa746bf693a44a685345687e39e9a80829c413e
@Derrick <dimatata01@gmail.com>
2025-04-22 19:55:16 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': os.getenv('DB_NAME'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
