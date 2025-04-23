cd625e3379fee7a051a3d95666d053272bb59034
@Derrick <dimatata01@gmail.com>
2025-04-23 19:59:38 +0300

    update

--- api/urls.py
+++ api/urls.py
@@ -13,11 +13,11 @@ router.register(r'customers', CustomerViewSet)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
     path('api/', include(router.urls)),
     path('oidc/login/', CustomLoginView.as_view(), name='oidc-login'),
     path('oidc/callback/', CustomOIDCAuthenticationCallbackView.as_view(), 
          name='oidc_authentication_callback'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
