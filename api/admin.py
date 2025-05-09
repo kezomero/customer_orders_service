fd7774a29481f80ef9ffb4892e3f469da7aee1a9
@Derrick <dimatata01@gmail.com>
2025-05-09 16:50:32 +0300

    update

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Customer, Order
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'email', 'phone', 'location', 'joined_at')
+    search_fields = ('name', 'code', 'email', 'phone')
+    list_filter = ('joined_at',)
+    ordering = ('-joined_at',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'item', 'quantity', 'amount', 'total_cost', 'payment_method', 'created_at')
+    search_fields = ('customer__name', 'item', 'payment_method')
+    list_filter = ('payment_method', 'created_at')
+    ordering = ('-created_at',)
+
+    def total_cost(self, obj):
+        return obj.total_cost
+    total_cost.short_description = 'Total Cost'
