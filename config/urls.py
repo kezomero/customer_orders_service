ef5ec930bb6ccdf7087befa966ecc09c419e2337
@Derrick <dimatata01@gmail.com>
2025-05-09 16:55:51 +0300

    update

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 # Swagger Schema View
 schema_view = get_schema_view(
@@ -13,6 +14,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="Derick Ingairo"),
    ),
    public=True,
+   permission_classes=(permissions.AllowAny,), 
 )
 
 urlpatterns = [
