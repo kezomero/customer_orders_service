4299651688cdd789be13c0d965a121bcae415bb0
@Derrick <dimatata01@gmail.com>
2025-05-11 12:47:53 +0300

    update

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api/', include(router.urls)),
     path('oidc/login/', CustomLoginView.as_view(), name='oidc-login'),
+    path('oidc/logout/', logout_view, name='oidc_logout'),
     path('oidc/callback/', CustomOIDCAuthenticationCallbackView.as_view(), name='oidc_authentication_callback'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
