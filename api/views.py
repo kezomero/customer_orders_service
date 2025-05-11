2e8094fb368abb0998d133151c627e62e15baf31
@Derrick <dimatata01@gmail.com>
2025-05-11 14:10:15 +0300

    update

--- api/views.py
+++ api/views.py
@@ -101,9 +101,15 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         try:
-            return super().update(request, *args, **kwargs)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data)
+            if serializer.is_valid():
+                self.perform_update(serializer)
+                return Response({'message': 'Customer updated successfully.', 'customer': serializer.data}, status=status.HTTP_200_OK)
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Customer.DoesNotExist:
             return Response({'error': 'Customer not found.'}, status=status.HTTP_404_NOT_FOUND)
+
 
     def destroy(self, request, *args, **kwargs):
         try:
