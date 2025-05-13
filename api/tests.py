f322048e9a15e6a32d7fa1fd2fe02708be50bc48
@Derrick <dimatata01@gmail.com>
2025-05-13 17:05:22 +0300

    update

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,7 @@
 import logging
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Customer, Order
 from unittest.mock import patch
