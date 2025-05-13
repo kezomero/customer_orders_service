f9751e73951162f875fb199395ff702f4dc76fee
@Derrick <dimatata01@gmail.com>
2025-05-13 14:10:27 +0300

    update

--- api/views.py
+++ api/views.py
@@ -33,6 +33,7 @@ class CustomOIDCAuthenticationCallbackView(OIDCAuthenticationCallbackView):
             "id": user.id,
             "username": user.username,
             "email": user.email,
+            "name": user.name,
             # "first_name": user.first_name,
             # "last_name": user.last_name,
             # "code_id": getattr(user, "code_id", None),
