c2f6eadc428bd2487a16f8f2b14677f165daf348
@Derrick <dimatata01@gmail.com>
2025-05-13 14:16:52 +0300

    update

--- api/views.py
+++ api/views.py
@@ -25,6 +25,9 @@ logger = logging.getLogger(__name__)
 # OIDC Callback View - Customizing token generation on successful login
 class CustomOIDCAuthenticationCallbackView(OIDCAuthenticationCallbackView):
     def get(self, request, *args, **kwargs):
+        print("User:", request.user)
+        print("Is Authenticated:", request.user.is_authenticated)
+        
         response = super().get(request, *args, **kwargs)
 
         user = request.user
